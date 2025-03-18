634e2ed7dac70182f43ccda0b598724837af0e97
Prisca-K <uzoanioke2017@gmail.com>
2025-03-18 15:27:37 +0100

    Job/project Update

--- app/Http/Resources/SwifthayajobResource.php
+++ app/Http/Resources/SwifthayajobResource.php
@@ -18,16 +18,6 @@ class SwifthayajobResource extends JsonResource
       'id' => $this->id,
       'company_id' => $this->company_id,
       'title' => $this->title,
-<<<<<<< HEAD
-      'description' => $this->description,
-      'required_skills' => $this->required_skills,
-      'location' => $this->location,
-      'salary_min' => $this->salary_min,
-      'salary_max' => $this->salary_max,
-      'job_type' => $this->job_type,
-      'posted_at' => $this->posted_at,
-      'deadline_date' => (is_null($this->deadline_date)) ? $this->deadline_date : $this->deadline_date->toDateTimeString(),
-=======
       'location' => $this->location,
       'salary' => $this->salary_amount ? [
         'amount' => $this->salary_amount,
@@ -39,7 +29,6 @@ class SwifthayajobResource extends JsonResource
       'qualifications' => $this->qualifications,
       'experience_level' => $this->experience_level,
       'job_type' => $this->job_type,
->>>>>>> d8307de (Recovering lost project)
       'status' => $this->status,
     ];
   }
